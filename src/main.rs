7bd566dd8353fc782c6925884c772e1c22a00647
Eric Smith <paytonrules@gmail.com>
2020-07-03 00:48:49 -0500

    Finish command line blackjack game.
    It's funky and untested, but hey it's playable.

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,12 @@ fn main() -> Result<(), Box<dyn Error>> {
         match &state {
             GameState::Ready(_) => {
                 state = deal(&state)?;
-            },
+            }
             GameState::WaitingForPlayer(context) => {
-                println!( "Dealer shows {:?}", context.dealer_hand.upcard().unwrap().rank);
+                println!(
+                    "Dealer shows {:?}",
+                    context.dealer_hand.upcard().unwrap().rank
+                );
                 print!("You have ");
                 for card in context.player_hand.cards() {
                     print!("{:?} ", card.rank)
@@ -26,27 +29,50 @@ fn main() -> Result<(), Box<dyn Error>> {
                 io::stdin()
                     .read_line(&mut command)
                     .expect("Failed to read line");
-                
+
                 match command.trim() {
                     "H" | "h" => state = hit(&state)?,
                     "S" | "s" => state = stand(&state)?,
-                    _ => { println!("Please try again"); state = state }
+                    _ => {
+                        println!("Please try again");
+                        state = state
+                    }
                 };
             }
-            GameState::DealerWins(_) => {
-                println!("Dealer Wins!");
-                break;
-            }
-            GameState::PlayerWins(_) => {
-                println!("Blackjack!");
-                break;
-            }
-            GameState::Draw(_) => {
-                println!("It's a tie, womp womp, you loser");
-                break;
+            GameState::DealerWins(context)
+            | GameState::PlayerWins(context)
+            | GameState::Draw(context) => {
+                print!("Dealer has ");
+                for card in context.dealer_hand.cards() {
+                    print!("{:?} ", card.rank);
+                }
+                println!("");
+                println!("For a total of {:?}", context.dealer_hand.score().0);
+                print!("You have ");
+                for card in context.player_hand.cards() {
+                    print!("{:?} ", card.rank)
+                }
+                println!("");
+                println!("For a total of {:?}", context.player_hand.score().0);
+                match state {
+                    GameState::DealerWins(_) => println!("Dealer Wins!"),
+                    GameState::PlayerWins(_) => println!("Player Wins!"),
+                    GameState::Draw(_) => println!("Tie. Womp womp"),
+                    _ => panic!("Impossible state reached"),
+                }
+                println!("Another hand?");
+                let mut command = String::new();
+                io::stdin()
+                    .read_line(&mut command)
+                    .expect("Failed to read line");
+
+                match command.trim() {
+                    "Y" | "y" => state = deal(&state)?,
+                    _ => break,
+                }
             }
         };
-    };
+    }
 
     Ok(())
 }
