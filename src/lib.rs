db2b37a27041a5fb95b0ad558cf0244ecfeeb454
Eric Smith <paytonrules@gmail.com>
2020-08-11 08:53:00 -0500

    Show the rank of the card when dealing
    Still only deals one card.

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,30 @@ use gdnative::prelude::*;
 mod deck;
 mod game;
 mod hand;
+use deck::{Card, Rank};
 use game::{deal, GameState};
+
+fn rank_as_texture_abbreviation(rank: &Rank) -> String {
+    match rank {
+        Rank::Two
+        | Rank::Three
+        | Rank::Four
+        | Rank::Five
+        | Rank::Six
+        | Rank::Seven
+        | Rank::Eight
+        | Rank::Nine
+        | Rank::Ten => format!("{}", rank.to_value().0),
+        Rank::Ace => "A".to_string(),
+        Rank::Jack => "J".to_string(),
+        Rank::Queen => "Q".to_string(),
+        Rank::King => "K".to_string(),
+    }
+}
+
+fn card_texture_from_card(card: &Card) -> String {
+    format!("cardDiamonds{}", rank_as_texture_abbreviation(&card.rank))
+}
 
 #[derive(NativeClass)]
 #[inherit(Node2D)]
@@ -24,18 +47,23 @@ impl Gui {
         match state {
             GameState::WaitingForPlayer(context) => {
                 godot_print!("I dealt some cards including {:?}", context.player_hand);
-                let first_hand = Sprite::new();
+                for card in context.player_hand.cards() {
+                    let first_hand = Sprite::new();
 
-                let sprite_name = "res://images/playingCards.cardClubsJ.atlastex";
-                let resource_loader = ResourceLoader::godot_singleton();
-                let texture = resource_loader
-                    .load(sprite_name, "AtlasTexture", false)
-                    .and_then(|res| res.cast::<AtlasTexture>())
-                    .expect("Couldn't load atlasTexture texture");
+                    let sprite_name = format!(
+                        "res://images/playingCards.{}.atlastex",
+                        card_texture_from_card(&card),
+                    );
+                    let resource_loader = ResourceLoader::godot_singleton();
+                    let texture = resource_loader
+                        .load(sprite_name, "AtlasTexture", false)
+                        .and_then(|res| res.cast::<AtlasTexture>())
+                        .expect("Couldn't load atlasTexture texture");
 
-                first_hand.set_texture(texture);
-                first_hand.set_position(Vector2::new(71.0, 200.0));
-                owner.add_child(first_hand, false);
+                    first_hand.set_texture(texture);
+                    first_hand.set_position(Vector2::new(71.0, 200.0));
+                    owner.add_child(first_hand, false);
+                }
             }
             _ => {
                 godot_print!("New state I didn't expect");
@@ -49,3 +77,75 @@ fn init(handle: InitHandle) {
 }
 
 godot_init!(init);
+
+#[cfg(test)]
+mod godot_lib {
+    use super::*;
+    use deck::{Card, Rank, Suit};
+
+    #[test]
+    fn two_of_diamonds_resource_string_from_card() {
+        let card = Card {
+            rank: Rank::Two,
+            suit: Suit::Diamond,
+        };
+        let resource = card_texture_from_card(&card);
+
+        assert_eq!("cardDiamonds2", resource);
+    }
+
+    #[test]
+    fn three_of_diamonds_resource_string_from_card() {
+        let card = Card {
+            rank: Rank::Three,
+            suit: Suit::Diamond,
+        };
+        let resource = card_texture_from_card(&card);
+
+        assert_eq!("cardDiamonds3", resource);
+    }
+
+    #[test]
+    fn ace_of_diamonds_resource_string_from_card() {
+        let card = Card {
+            rank: Rank::Ace,
+            suit: Suit::Diamond,
+        };
+        let resource = card_texture_from_card(&card);
+
+        assert_eq!("cardDiamondsA", resource);
+    }
+
+    #[test]
+    fn king_of_diamonds_resource_string_from_card() {
+        let card = Card {
+            rank: Rank::King,
+            suit: Suit::Diamond,
+        };
+        let resource = card_texture_from_card(&card);
+
+        assert_eq!("cardDiamondsK", resource);
+    }
+
+    #[test]
+    fn queen_of_diamonds_resource_string_from_card() {
+        let card = Card {
+            rank: Rank::Queen,
+            suit: Suit::Diamond,
+        };
+        let resource = card_texture_from_card(&card);
+
+        assert_eq!("cardDiamondsQ", resource);
+    }
+
+    #[test]
+    fn jack_of_diamonds_resource_string_from_card() {
+        let card = Card {
+            rank: Rank::Jack,
+            suit: Suit::Diamond,
+        };
+        let resource = card_texture_from_card(&card);
+
+        assert_eq!("cardDiamondsJ", resource);
+    }
+}
