6dad985759c376ef27e81b1f42b97d20267bbbe2
Eric Smith <paytonrules@gmail.com>
2020-08-13 08:03:19 -0500

    Show both hands
    Note the dealer shows their hole card. Also note this code is a dumpster
    fire filled with poop, and desperately needs refactoring. BUT FORWARD WE
    SHALL GO!
    
    Right now the dealer hand and player hand are scenes. I am not sure this
    is necessary, but I suspect it will help with animations later.

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,26 @@ mod game;
 mod hand;
 use deck::{Card, Rank};
 use game::{deal, GameState};
+use std::error;
+
+pub fn get_typed_node<O, F>(name: &str, owner: &Node, mut f: F)
+where
+    F: FnMut(TRef<O>),
+    O: GodotObject + SubClass<Node>,
+{
+    let node = match owner
+        .get_node(name)
+        .map(|node| unsafe { node.assume_safe() })
+        .and_then(|node| node.cast::<O>())
+    {
+        Some(it) => it,
+        _ => {
+            godot_print!("Couldn't find node {:?}", name);
+            return;
+        }
+    };
+    f(node)
+}
 
 fn rank_as_texture_abbreviation(rank: &Rank) -> String {
     match rank {
@@ -34,6 +54,17 @@ fn card_texture_from_card(card: &Card) -> String {
 
 #[derive(NativeClass)]
 #[inherit(Node2D)]
+struct Hand {}
+
+#[methods]
+impl Hand {
+    fn new(_owner: &Node2D) -> Self {
+        Hand {}
+    }
+}
+
+#[derive(NativeClass)]
+#[inherit(Node2D)]
 struct Blackjack {
     state: GameState,
 }
@@ -52,33 +83,60 @@ impl Blackjack {
 
         match &self.state {
             GameState::WaitingForPlayer(context) => {
-                for card in context.player_hand.cards() {
-                    let first_hand = Sprite::new();
-
-                    let sprite_name = format!(
-                        "res://images/playingCards.{}.atlastex",
-                        card_texture_from_card(&card),
-                    );
+                get_typed_node::<Node2D, _>("./PlayerHand", owner, |player_hand| {
                     let resource_loader = ResourceLoader::godot_singleton();
-                    let texture = resource_loader
-                        .load(sprite_name, "AtlasTexture", false)
-                        .and_then(|res| res.cast::<AtlasTexture>())
-                        .expect("Couldn't load atlasTexture texture");
+                    for card in context.player_hand.cards() {
+                        let sprite = Sprite::new();
 
-                    first_hand.set_texture(texture);
-                    first_hand.set_position(Vector2::new(71.0, 200.0));
-                    owner.add_child(first_hand, false);
-                }
+                        let sprite_name = format!(
+                            "res://images/playingCards.{}.atlastex",
+                            card_texture_from_card(&card),
+                        );
+                        let texture = resource_loader
+                            .load(sprite_name, "AtlasTexture", false)
+                            .and_then(|res| res.cast::<AtlasTexture>())
+                            .expect("Couldn't load atlasTexture texture");
+
+                        let child_count = player_hand.get_child_count() as f32;
+                        sprite.set_texture(texture);
+                        sprite.set_position(Vector2::new(child_count * 70.0, 0.0));
+                        player_hand.add_child(sprite, false);
+                    }
+                });
+
+                get_typed_node::<Node2D, _>("./DealerHand", owner, |dealer_hand| {
+                    let resource_loader = ResourceLoader::godot_singleton();
+                    for card in context.dealer_hand.cards() {
+                        let sprite = Sprite::new();
+
+                        let sprite_name = format!(
+                            "res://images/playingCards.{}.atlastex",
+                            card_texture_from_card(&card),
+                        );
+                        let texture = resource_loader
+                            .load(sprite_name, "AtlasTexture", false)
+                            .and_then(|res| res.cast::<AtlasTexture>())
+                            .expect("Couldn't load atlasTexture texture");
+
+                        let child_count = dealer_hand.get_child_count() as f32;
+                        sprite.set_texture(texture);
+                        sprite.set_position(Vector2::new(child_count * 70.0, 0.0));
+                        dealer_hand.add_child(sprite, false);
+                    }
+                });
             }
-            _ => {
-                godot_print!("New state I didn't expect");
-            }
+
+            GameState::Ready(_) => {}
+            GameState::DealerWins(_) => {}
+            GameState::PlayerWins(_) => {}
+            GameState::Draw(_) => {}
         }
     }
 }
 
 fn init(handle: InitHandle) {
     handle.add_class::<Blackjack>();
+    handle.add_class::<Hand>();
 }
 
 godot_init!(init);
