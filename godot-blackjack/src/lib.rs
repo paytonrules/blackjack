88218e46bb98f72ef256fe2eaca01cf4e72f216d
Eric Smith <paytonrules@gmail.com>
2020-08-23 10:53:47 -0500

    Rewrite animation for dealing to use tweens
    This is much simpler, and cards are dealing to the right spot.

--- godot-blackjack/src/lib.rs
+++ godot-blackjack/src/lib.rs
@@ -3,32 +3,8 @@ use blackjack::{
     game::{deal, hit, stand, GameState},
     hand::{DealerHand, Hand},
 };
-use gdnative::api::{Animation, AnimationPlayer, AtlasTexture, RichTextLabel, ToolButton};
+use gdnative::api::{AtlasTexture, RichTextLabel, ToolButton};
 use gdnative::prelude::*;
-
-fn load_scene<F, T>(name: &str, mut f: F) -> Option<T>
-where
-    F: FnMut(TRef<PackedScene>) -> Option<T>,
-{
-    let scene = ResourceLoader::godot_singleton().load(name, "PackedScene", false)?;
-    let scene = unsafe { scene.assume_safe() };
-    let packed_scene = scene.cast::<PackedScene>()?;
-
-    f(packed_scene)
-}
-
-fn create_node_from_scene<T>(name: &str) -> Option<Ref<T>>
-where
-    T: GodotObject<RefKind = ManuallyManaged> + SubClass<Node>,
-{
-    load_scene(name, |scene| {
-        scene
-            .instance(PackedScene::GEN_EDIT_STATE_DISABLED)
-            .map(|node| unsafe { node.assume_unique() })
-            .and_then(|node| node.cast::<T>())
-            .map(|node| node.into_shared())
-    })
-}
 
 fn clear_all_children(node_name: &str, owner: &Node) {
     get_typed_node::<Node, _>(node_name, owner, |parent| {
@@ -88,8 +64,35 @@ fn texture_path_from_card(card: &Card) -> String {
     )
 }
 
-fn add_card_to_hand(owner: &Node2D, texture: &str, hand: &Node2D) {
-    play_deal_animation(owner, &hand, texture);
+fn add_card_to_hand(texture: &str, hand: &Node2D) {
+    let resource_loader = ResourceLoader::godot_singleton();
+    let sprite = Sprite::new();
+    let texture = resource_loader
+        .load(texture, "AtlasTexture", false)
+        .and_then(|res| res.cast::<AtlasTexture>())
+        .expect("Couldn't load atlasTexture texture");
+
+    let child_count = hand.get_child_count() as f32;
+    sprite.set_texture(texture);
+    let sprite = unsafe { sprite.assume_shared() };
+    hand.add_child(sprite, false);
+
+    let tween = Tween::new();
+    tween.interpolate_property(
+        sprite,
+        "position",
+        Vector2::new(-hand.position().x, -hand.position().y),
+        Vector2::new(child_count * 35.0, 0.0),
+        0.25,
+        Tween::TRANS_LINEAR,
+        Tween::EASE_IN,
+        0.0,
+    );
+
+    let tween = unsafe { tween.assume_shared() };
+    hand.add_child(tween, false);
+    let tween = unsafe { tween.assume_safe() };
+    tween.start();
 }
 
 fn show_dealer_hole_card(texture: &str, hand: &Node2D) {
@@ -111,7 +114,7 @@ fn show_dealer_hole_card(texture: &str, hand: &Node2D) {
 fn show_player_hand(owner: &Node2D, player_hand: &Hand) {
     get_typed_node::<Node2D, _>("./PlayerHand", owner, |node| {
         for card in player_hand.cards() {
-            add_card_to_hand(owner, &texture_path_from_card(&card), &node);
+            add_card_to_hand(&texture_path_from_card(&card), &node);
         }
     });
 }
@@ -119,13 +122,11 @@ fn show_player_hand(owner: &Node2D, player_hand: &Hand) {
 fn show_initial_dealer_hand(owner: &Node2D, dealer_hand: &DealerHand) {
     get_typed_node::<Node2D, _>("./DealerHand", owner, |node| {
         add_card_to_hand(
-            owner,
             "res://images/playingCardBacks.cardBack_blue1.atlastex",
             &node,
         );
 
         add_card_to_hand(
-            owner,
             &texture_path_from_card(&dealer_hand.upcard().unwrap()),
             &node,
         );
@@ -140,7 +141,7 @@ fn show_remaining_dealer_hand(owner: &Node2D, dealer_hand: &DealerHand) {
         );
 
         for card in dealer_hand.cards().skip(2) {
-            add_card_to_hand(owner, &texture_path_from_card(&card), &node);
+            add_card_to_hand(&texture_path_from_card(&card), &node);
         }
     });
 }
@@ -148,7 +149,7 @@ fn show_remaining_dealer_hand(owner: &Node2D, dealer_hand: &DealerHand) {
 fn show_entire_dealer_hand(owner: &Node2D, dealer_hand: &DealerHand) {
     get_typed_node::<Node2D, _>("./DealerHand", owner, |node| {
         for card in dealer_hand.cards() {
-            add_card_to_hand(owner, &texture_path_from_card(&card), &node);
+            add_card_to_hand(&texture_path_from_card(&card), &node);
         }
     });
 }
@@ -157,7 +158,7 @@ fn show_latest_player_card(owner: &Node2D, player_hand: &Hand) {
     get_typed_node::<Node2D, _>("./PlayerHand", owner, |node| {
         let player_cards = player_hand.cards();
         let new_card = player_cards.last().unwrap();
-        add_card_to_hand(owner, &texture_path_from_card(&new_card), &node);
+        add_card_to_hand(&texture_path_from_card(&new_card), &node);
     })
 }
 
@@ -171,47 +172,6 @@ fn clear_result_text(owner: &Node2D) {
     get_typed_node::<RichTextLabel, _>("./Result", owner, |node| {
         node.clear();
     });
-}
-
-fn play_deal_animation(
-    owner: &Node2D,
-    hand: &Node2D,
-    texture: &str,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let fly_in_node = create_node_from_scene::<Node2D>("res://CardFlyIn.tscn")
-        .ok_or("Could Not Create Node From Scene res://CardFlyIn.tscn")?;
-    let fly_in_node = unsafe { fly_in_node.assume_safe() };
-
-    get_typed_node::<AnimationPlayer, _>("./CardAnimation", &fly_in_node, |player| {
-        let animation = player
-            .get_animation("Card Fly In")
-            .expect("Could Not find 'Card Fly In' animation");
-        let animation = unsafe { animation.assume_safe() };
-
-        animation.track_set_key_value(0, 1, hand.position());
-
-        owner.add_child(fly_in_node, false);
-
-        let binds = VariantArray::new();
-        binds.push(texture);
-        let hand = unsafe { hand.assume_shared() };
-        binds.push(hand);
-        let fly_in_node = unsafe { fly_in_node.assume_shared() };
-        binds.push(fly_in_node);
-        let owner = unsafe { owner.assume_shared() };
-        player
-            .connect(
-                "animation_finished",
-                owner,
-                "card_dealt",
-                binds.into_shared(),
-                0,
-            )
-            .expect("This is some next level nonsense");
-
-        player.play("Card Fly In", -1.0, 1.0, false);
-    });
-    Ok(())
 }
 
 #[derive(NativeClass)]
@@ -337,33 +297,6 @@ impl Blackjack {
                 });
             }
         }
-    }
-
-    #[export]
-    fn card_dealt(
-        &self,
-        owner: &Node2D,
-        _anim_name: String,
-        texture: String,
-        hand: Ref<Node2D>,
-        fly_in_node: Ref<Node2D>,
-    ) {
-        owner.remove_child(fly_in_node);
-        let fly_in_node = unsafe { fly_in_node.assume_unique() };
-        fly_in_node.queue_free();
-
-        let hand = unsafe { hand.assume_safe() };
-        let resource_loader = ResourceLoader::godot_singleton();
-        let sprite = Sprite::new();
-        let texture = resource_loader
-            .load(texture, "AtlasTexture", false)
-            .and_then(|res| res.cast::<AtlasTexture>())
-            .expect("Couldn't load atlasTexture texture");
-
-        let child_count = hand.get_child_count() as f32;
-        sprite.set_texture(texture);
-        sprite.set_position(Vector2::new(child_count * 70.0, 0.0));
-        hand.add_child(sprite, false);
     }
 }
 
